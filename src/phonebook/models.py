d31fa22c1d58e2728e9a51ef4fe9d6dfadc9c9fa
Петров Евгений Викторович <mkultimate0@gmail.com>
2025-12-11 17:19:03 +0500

    добавли поле 'subdivision(подразделение)' в модель Persone

--- src/phonebook/models.py
+++ src/phonebook/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Persone(models.Model):
 	# pk/id
 	name = models.CharField("Contact name", max_length=50)
-
+	subdivision = models.CharField("Subdivision name", max_length=100)
+	
 	def __str__(self):
 		return self.name
 
